bce0f50953088ba07d35723d943a3902e80af422
jus074 <jus074@eng.ucsd.edu>
2017-11-30 19:46:07 -0800

    add login form

--- around/src/components/Main.js
+++ around/src/components/Main.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Register } from './Register';
 // import './Main.css';
-
+import { Login } from './Login';
 export class Main extends React.Component {
   render() {
     return (
         <section className="main">
-          <Register/>
+          {/*<Register/>*/}
+          <Login/>
         </section>
     );
   }
