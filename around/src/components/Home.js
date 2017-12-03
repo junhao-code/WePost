2bcdb03736738a0596862f7c957eb50e0f8fde6f
jus074 <jus074@eng.ucsd.edu>
2017-12-03 14:46:44 -0800

    added geo_location permission

--- around/src/components/Home.js
+++ around/src/components/Home.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
+import {GEO_OPTIONS} from "../constants"
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+      navigator.geolocation.getCurrentPosition(
+          this.onSuccessLoadGeoLocation,
+          this.onFailedLoadGeoLocation,
+          GEO_OPTIONS,
+      );
+    } else {
+      console.log("geo location not supported!");
+    }
+  }
+  onSuccessLoadGeoLocation = (position) => {
+    console.log(position);
+  }
+
+  onFailedLoadGeoLocation = () => {
+
+  }
   render() {
     return (
         <Tabs tabBarExtraContent={operations} className="main-tabs">
