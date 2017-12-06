6bf1cee06f476002aeb2ccabc37e7ee4fdc6b244
jus074 <jus074@eng.ucsd.edu>
2017-12-05 23:22:51 -0800

    finished stage 4

--- around/src/components/AroundMap.js
+++ around/src/components/AroundMap.js
@@ -5,10 +5,22 @@ import { AroundMarker } from "./AroundMarker"
 
 class AroundMap extends React.Component {
 
+  onDragEnd = () => {
+    const center = this.map.getCenter();
+    const position = { lat: center.lat(), lon: center.lng()};
+    localStorage.setItem(POS_KEY, JSON.stringify(position));
+    this.props.loadNearbyPosts();
+  }
+  getMapRef = (map) => {
+    this.map = map;
+  }
+
   render() {
     const pos = JSON.parse(localStorage.getItem(POS_KEY));
     return(
         <GoogleMap
+            onDragEnd={this.onDragEnd}
+            ref={this.getMapRef}
             defaultZoom={11}
             defaultCenter={{ lat: pos.lat, lng: pos.lon }}
         >
--- around/src/components/CreatePostButton.js
+++ around/src/components/CreatePostButton.js
@@ -32,8 +32,8 @@ export class CreatePostButton extends React.Component {
       // prepare form data
       const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
       const formData = new FormData();
-      formData.set('lat', lat + Math.random() * 0.1 - 0.05);
-      formData.set('lon', lon + Math.random() * 0.1 - 0.05);
+      formData.set('lat', lat + Math.random() * 0.05 - 0.1);
+      formData.set('lon', lon + Math.random() * 0.05 - 0.1);
       formData.set('message', form.getFieldValue('message'));
       formData.set('image', form.getFieldValue('image')[0]);
 
