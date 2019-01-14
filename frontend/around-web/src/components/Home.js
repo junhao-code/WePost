a7615d506299e448040aef35a050d3afaf9c9a2e
jus074 <jus074@eng.ucsd.edu>
2019-01-13 22:18:57 -0800

    geolocation spinner added

--- frontend/around-web/src/components/Home.js
+++ frontend/around-web/src/components/Home.js
@@ -1,11 +1,56 @@
 import React from 'react';
-import { Tabs, Button } from 'antd';
+import { Tabs, Button, Spin} from 'antd';
+import { GEO_OPTIONS, POS_KEY} from "../constants"
 
 const TabPane = Tabs.TabPane;
 
 const operations = <Button>New Post</Button>;
 
 export class Home extends React.Component {
+
+  state = {
+    loadingGeolocation: false,
+  }
+  // After componentdidmount in the lifecycle, we will load posts
+  // componentDidMount will only be executed at the beginning, thus we decide that we only have to get geolocation from the beginning
+  // no modification is necessary later on.
+  componentDidMount() {
+    if ("geolocation" in navigator) {
+      /* geolocation is available */
+      this.setState({loadingGeolocation : true});
+      navigator.geolocation.getCurrentPosition(
+          this.onSuccessLoadGeolocation,
+          this.onFailedLoadGeolocation,
+          GEO_OPTIONS,
+      );
+    } else {
+      /* geolocation IS NOT available */
+    }
+  }
+
+  onSuccessLoadGeolocation = (position) => {
+    console.log(position);
+    // destructuring coords
+    const {latitude: lat, longitude: lon} = position.coords;
+    // if {lat: lat, lon: lon} are saved without stringify, they will be saved as objects
+    localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon: lon}));
+    this.setState({loadingGeolocation : false});
+
+  }
+
+  onFailedLoadGeolocation = () => {
+    this.setState({loadingGeolocation : false});
+  }
+
+  getGalleryPanelContent = () => {
+    if (this.state.loadingGeolocation) {
+      // show spinner
+      return <Spin tip="Loading Geolocation..."></Spin>;
+
+    }
+    return null;
+  }
+
   render() {
     return (
         <Tabs
@@ -13,7 +58,7 @@ export class Home extends React.Component {
             className="main-tabs"
         >
           <TabPane tab="Posts" key="1">
-            Content of tab 1
+            {this.getGalleryPanelContent()}
           </TabPane>
           <TabPane tab="Map" key="2">
             Content of tab 2
--- frontend/around-web/src/constants.js
+++ frontend/around-web/src/constants.js
@@ -1,4 +1,11 @@
 export const API_ROOT = 'https://around-75015.appspot.com/api/v1';
 export const TOKEN_KEY = "TOKEN_KEY";
+export const POS_KEY = 'POS_KEY';
+export const GEO_OPTIONS = {
+  enableHighAccuracy: true,
+  maximumAge: 3600000,
+  timeout: 27000,
+};
+export const AUTH_PREFIX = 'Bearer';
 // export const proxyurl = "https://cors-anywhere.herokuapp.com/";
 // export const API_ROOT = 'https://around-227710.appspot.com/api/v1';
